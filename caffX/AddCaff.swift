522af89361ffb358fc63dc192d0aae7c6340f82d
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2024-08-19 13:29:05 +0800

    Added graph and basics

---
+++ caffX/AddCaff.swift
@@ -0,0 +1,18 @@
+//
+//  AddCaff.swift
+//  caffX
+//
+//  Created by Avyan Mehra on 19/8/24.
+//
+
+import SwiftUI
+
+struct AddCaff: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCaff()
+}
