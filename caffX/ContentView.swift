90b963102fc5ca8e5a1dc3d68d23dace8c7f16e5
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2024-08-20 11:26:29 +0800

    Added graph stuff

--- caffX/ContentView.swift
+++ caffX/ContentView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 import Charts
 
+
 struct ContentView: View {
+    @State var caffIntake = [20,18,16,116,100,89,78,64,29,47]
     var body: some View {
         NavigationStack {
-            Text("Hello World")
-            
+            Text("Today's Intake")
+                .font(.largeTitle)
+                .fontWeight(.medium)
+            Chart {
+                ForEach(0..<caffIntake.count, id: \.self) { entry in
+                    LineMark(
+                        x: .value("Hour",entry+1),
+                        y: .value("Ca",caffIntake[entry])
+                    )
+                
+                }
+            }
+            .frame(width: 300, height: 200)
+            .padding()
                 .navigationTitle("caffX")
                 .navigationBarTitleDisplayMode(.large)
+            
+            
+            Spacer()
         }
         .padding()
     }
