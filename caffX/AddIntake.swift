31e2f98be9de3d447addeeb68168c4a11ae52a17
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2024-08-20 14:47:27 +0800

    Added list and added a page to add caff intake

---
+++ caffX/AddIntake.swift
@@ -0,0 +1,18 @@
+//
+//  AddIntake.swift
+//  caffX
+//
+//  Created by Avyan Mehra on 20/8/24.
+//
+
+import SwiftUI
+
+struct AddIntake: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddIntake()
+}
---
+++ caffX/drinkList.swift
@@ -0,0 +1,8 @@
+//
+//  drinkList.swift
+//  caffX
+//
+//  Created by Avyan Mehra on 20/8/24.
+//
+
+import Foundation
