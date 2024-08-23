33713f381f9b89339c5d0b982f9a5973ca059021
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2024-08-23 08:40:05 +0800

    tried other graph mehtod

---
+++ caffX/GraphView.swift
@@ -0,0 +1,18 @@
+//
+//  GraphView.swift
+//  caffX
+//
+//  Created by Avyan Mehra on 23/8/24.
+//
+
+import SwiftUI
+
+struct GraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GraphView()
+}
